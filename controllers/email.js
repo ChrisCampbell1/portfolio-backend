9dc9e215e913d64e06139d0f547def9b982483c1
Chris Campbell <christopherecampbell@gmail.com>
2023-03-19 17:06:36 -0600

    add email controller

---
+++ controllers/email.js
@@ -0,0 +1,7 @@
+function sendEmail() {
+
+}
+
+export {
+  sendEmail
+}
--- routes/email.js
+++ routes/email.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
+import * as emailController from '../controllers/email.js'
 
 const router = Router()
 
-// GET localhost:3000/users
 router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
